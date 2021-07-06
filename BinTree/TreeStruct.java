3f3013f969548167f5a77ade9e9c97629708f50a
Rilong Liao <lang19880211@163.com>
2021-07-06 22:29:34 +0800

    Update TreeStruct.java

--- BinTree/TreeStruct.java
+++ BinTree/TreeStruct.java
@@ -11,26 +11,116 @@ public class TreeStruct{
 		right=null;
 	}
 
+	//先序，使用栈的方式
+	public static void perOrderByStack(TreeStruct root,ArrayList<String> list){
+		Stack<TreeStruct> stack = new Stack<TreeStruct>();
 
+		stack.push(root);
+		TreeStruct curr = null;
 
-	public static void preOrder(TreeStruct root){
+		while(!stack.isEmpty()){
+			curr = stack.pop();
+			//System.out.print(curr.data+",");
+			list.add(curr.data);
+			if(curr.right!=null){
+				stack.push(curr.right);
+			}
+			if(curr.left!=null){
+				stack.push(curr.left);
+			}
+		}
+	}
+
+	//先序，递归方式
+	public static void preOrder(TreeStruct root,ArrayList<String> list){
 		if(root == null)
 			return;
-		System.out.print(root.data+",");
-		preOrder(root.left);
-		preOrder(root.right);
+		//System.out.print(root.data+",");
+		list.add(root.data);
+		preOrder(root.left,list);
+		preOrder(root.right,list);
 	}
+
+
+
+
+
+	//后序，递归方式
+	public static void lastOrder(TreeStruct root,ArrayList<String> list){
+		if(root == null) return;
+
+		lastOrder(root.left,list);
+		lastOrder(root.right,list);
+		list.add(root.data);
+	}
+
+	//后序，使用栈的方式
+	public static void lastOrderByStack(TreeStruct root,ArrayList<String> list){
+
+		Stack<TreeStruct> stack = new Stack<TreeStruct>();
+		Stack<TreeStruct> help = new Stack<TreeStruct>();
+		stack.push(root);
+		TreeStruct curr = null;
+		while(!stack.isEmpty()){
+			curr=stack.pop();
+			help.push(curr);
+			if(curr.left!=null){
+				stack.push(curr.left);
+			}
+			if(curr.right!=null){
+				stack.push(curr.right);
+			}
+		}
+
+		while(!help.isEmpty()){
+			list.add(help.pop().data);
+		}
+	}
+
+
+
+
+	//中序，递归方式
+	public static void inOrder(TreeStruct root,ArrayList<String> list){
+		if(root == null) return ;
+		inOrder(root.left,list);
+		list.add(root.data);
+		inOrder(root.right,list);
+	}
+
+	//中序，使用栈的方式
+	public static void inOrderByStack(TreeStruct root,ArrayList<String> list){
+		Stack<TreeStruct> stack = new Stack<TreeStruct>();
+		TreeStruct curr = root;
+
+		while(!stack.isEmpty() || curr != null){
+			if(curr!=null){
+				stack.push(curr);
+				curr = curr.left;
+			}
+			else{
+				curr = stack.pop();
+				list.add(curr.data);
+				curr = curr.right;
+			}
+		}
+	}
+
+
+
+
+
 
 	public static TreeStruct getNode(){
 		String v = Character.toString((char)('A'+((int)(Math.random()*26))));
 		return new TreeStruct(v);
 	}
 
-	public static void randomBinTree(TreeStruct root,int maxNodes){
+	public static void randomBinTree(TreeStruct root,int maxLevel){
 
 		TreeStruct p = root;
-		int left = maxNodes/2;
-		int right = maxNodes - left;
+		int left = maxLevel;
+		int right = maxLevel;
 
 		int random = (int)(Math.random()*9999);
 
@@ -64,12 +154,36 @@ public class TreeStruct{
 		}
 	}
 
+	public static Boolean compar(ArrayList<String> list,ArrayList<String> list2){
+		if(list.size()!= list2.size()) return false;
+		int size = list.size()-1;
+		while(size >0){
+			if(list.get(size) != list2.get(size--)) return false;
+		}
+		return true;
+	}
+
+	public static Boolean isTrue(){
+		int max = 1000000;
+		for (int i=0; i<max;i++ ) {
+			ArrayList<String> list = new ArrayList<String>();
+			ArrayList<String> list2 = new ArrayList<String>();
+			TreeStruct root = new TreeStruct("AAAA");
+			randomBinTree(root,500);
+			inOrder(root,list);
+			inOrderByStack(root,list2);
+			if(!compar(list,list2)){
+				return false;
+			}
+		}
+		return true;
+		
+	}
+
 	public static void main(String[] args) {
 
-
-		 TreeStruct root = new TreeStruct("AAAA");
-		randomBinTree(root,5);
-		preOrder(root);
+		System.out.print("result = "+isTrue());
+		
 	}
 
 }
