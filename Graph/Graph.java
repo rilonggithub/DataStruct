c94377195b58ae6cadf9aae3a217ba84e9ec6df2
Rilong Liao <lang19880211@163.com>
2021-07-23 23:35:59 +0800

    Update Graph.java

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,11 +1,12 @@
+  
 import java.util.*;
 public class Graph{
 	public static class Node{
-		private int value;
-		private int in;
-		private int to;
-		private List<Edge> edges;
-		private List<Node> nexts;
+		public int value;
+		public int in;
+		public int to;
+		public List<Edge> edges;
+		public List<Node> nexts;
 
 		public Node(int v,int i,int t,List<Edge> e,List<Node> n){
 			value=v;
@@ -13,19 +14,6 @@ public class Graph{
 			to =t;
 			edges=e;
 			nexts=n;
-		}
-
-		public void addEdge(Edge e){
-			edges.add(e);
-			to++;
-		}
-
-		public void addNext(Node n){
-			nexts.add(n);
-		}
-
-		public void addIn(){
-			in++;
 		}
 
 
@@ -51,41 +39,75 @@ public class Graph{
 		edges = new HashSet<>();
 	}
 
-	public void add(int[] arr){
-		Node f = nodes.get(arr[1]);
-		Node t = nodes.get(arr[2]);
-		Edge e = new Edge(arr[0],f,t); 
+	public void add(int[][] arr){
+		for (int i=0; i<arr.length; i++) {
+			Node f = nodes.get(arr[i][1]);
+			Node t = nodes.get(arr[i][2]);
+			Edge e = new Edge(arr[i][0],f,t); 
+			edges.add(e);
 
-		if(f==null){
-			List<Edge> eList = new ArrayList<>();
-			eList.add(e);
+			if(f==null){
 
-			List<Node> nList = new ArrayList<>();
-			nList.add(t);
+				List<Node> nList = new ArrayList<>();
+				List<Edge> eList = new ArrayList<>();
+				f = new Node(arr[i][1],0,1,eList,nList);
+				nodes.put(arr[i][1],f);
+			}
 
-			f = new Node(arr[1],0,1,eList,nList);
-		}else{
-			f.addEdge(e);
-			f.addNext(t);
+			if(t == null){
+				List<Edge> eList = new ArrayList<>();
+				List<Node> nList = new ArrayList<>();
+				t = new Node(arr[i][2],1,0,eList,nList);
+				nodes.put(arr[i][2],t);
+			}
+
+			f.edges.add(e);
+			f.nexts.add(t);
 		}
+		
+	}
 
-		if(t == null){
-			List<Edge> eList = new ArrayList<>();
-			List<Node> nList = new ArrayList<>();
-			t = new Node(arr[2],1,0,eList,nList);
-		}else{
-			t.addIn();
+
+	public void BFS(){
+		Queue<Node> queue = new LinkedList<>();
+		HashSet<Node> exist=new HashSet<>();
+		for (Node node : nodes.values() ) {
+			queue.offer(node);
+			while(!queue.isEmpty()){
+				Node cur = queue.poll();
+				if(!exist.contains(cur)){
+					exist.add(cur);
+					System.out.print(cur.value+",");
+					for (Node n : cur.nexts) {
+						queue.offer(n);
+					}
+				}
+			}
+			System.out.println();
 		}
 	}
 
 
 	public static void main(String[] args){
 		Graph g = new Graph();
-		int[] arr=new int[]{4,1,4};
-		arr=new int[]{4,3,4};
-		arr=new int[]{1,4,2};
-		arr=new int[]{9,2,1};
+		int[][] arr=new int[][]{
+			{4,1,4},
+			{4,4,2},
+			{4,3,4},
+			{4,2,1},
+			{4,4,5},
+			{4,1,3},
+			{4,12,17},
+			{4,3,7},
+			{4,5,9},
+			{4,2,8},
+			{4,5,4},
+			{4,2,3},
+			{4,8,17},
+			{4,2,7}
+		};
 		g.add(arr);
+		g.BFS();
 	}
 
 }
