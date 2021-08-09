50074b71560e854d300e617043645e4eec41864e
Rilong Liao <lang19880211@163.com>
2021-08-09 10:15:34 +0800

    Create SplitNumberProblem.java

---
+++ DP/SplitNumberProblem.java
@@ -0,0 +1,96 @@
+public class SplitNumberProblem{
+
+	public static int splitNum(int num){
+		int[] arr=new int[num];
+		for (int i=0; i<arr.length;i++ ) {
+			arr[i] = i+1;
+		}
+
+
+		return process(arr,num,0,"");
+	}
+
+
+	public static int process(int[] arr,int rest,int index,String path){
+
+		if(rest == 0){
+			//System.out.println(path);
+			return 1;
+		}
+
+		int ways=0;
+
+
+		for (int ci = index;  ci<arr.length&&rest - arr[ci]>=0; ci++) {
+			ways += process(arr,rest-arr[ci],ci,path+arr[ci]+",");
+			
+		}
+		return ways;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public static int splitNum2(int num){
+		int[] arr=new int[num];
+		for (int i=0; i<arr.length;i++ ) {
+			arr[i] = i+1;
+		}
+
+		int[][] dp = new int[num+1][num+1];
+		for (int i=0; i<=num; i++) {
+			for (int j=0; j<=num; j++) {
+				dp[i][j]=-1;
+			}
+		}
+		return process2(arr,num,0,"",dp);
+	}
+
+
+	public static int process2(int[] arr,int rest,int index,String path,int[][] dp){
+
+		if(dp[rest][index]!=-1){
+			return dp[rest][index];
+		}
+
+		if(rest == 0){
+			//System.out.println(path);
+			return 1;
+		}
+
+		int ways=0;
+
+
+		for (int ci = index;  ci<arr.length&&rest - arr[ci]>=0; ci++) {
+			ways += process2(arr,rest-arr[ci],ci,path+arr[ci]+",",dp);
+			
+		}
+		dp[rest][index] = ways;
+		return ways;
+	}
+
+	public static void main(String[] args){
+		int num = 100;
+		System.out.println("C = "+splitNum(num));
+		System.out.println("C = "+splitNum2(num));
+	}
+}
